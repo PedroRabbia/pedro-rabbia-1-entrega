ac68462af135740342ba0489c7f2a0150d628a52
PedroRabbia <106634257+PedroRabbia@users.noreply.github.com>
2023-04-04 15:59:20 -0300

    PRIMERA ENTREGA

---
+++ app.js
@@ -0,0 +1,14 @@
+import express from "express";
+import productRouter from "./routes/productRouter.js";
+import cartRouter from "./routes/cartRouter.js";
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/api/products", productRouter);
+app.use("/api/carts", cartRouter);
+
+app.listen(8084, () => {
+  console.log("Servidor Escuchando");
+});
